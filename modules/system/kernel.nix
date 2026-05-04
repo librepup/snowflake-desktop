6424761616542f9685618fd5c356e3d19e590f39
librepup <zweitlaender@gmail.com>
2026-05-05 00:16:50 +0200

    Fixed Copy.Fail Exploit Vulnerability

--- modules/system/kernel.nix
+++ modules/system/kernel.nix
@@ -3,4 +3,7 @@
   nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ];
   hardware.enableRedistributableFirmware = true;
   boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
+  boot.extraModprobeConfig = ''
+    install algif_aead /bin/false
+  '';
 }
