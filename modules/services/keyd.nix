a98ae9e48200c94f93402862309ae73777c8e57a
librepup <zweitlaender@gmail.com>
2026-05-27 17:13:49 +0200

    Updated KeyD Config

--- modules/services/keyd.nix
+++ modules/services/keyd.nix
@@ -8,16 +8,18 @@
         settings = {
           main = {
             rightshift = "leftmeta";
+            capslock = "leftcontrol";
           };
         };
       };
     };
   };
-  environment.etc."keyd/rShiftToSuper.conf".text = ''
+  environment.etc."keyd/config.conf".text = ''
     [ids]
     *
 
     [main]
     rightshift = leftmeta
+    capslock = leftcontrol
   '';
 }
