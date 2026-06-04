157bb1d13504f77aed4b5f7d4083e8fce83c29d1
librepup <zweitlaender@gmail.com>
2026-06-04 20:58:11 +0200

    Fixed Image Preview Script

--- files/scripts/previewImagesInTerm.sh
+++ files/scripts/previewImagesInTerm.sh
@@ -1,7 +1,6 @@
-#!/usr/bin/env bash
+#!/usr/bin/env bash]
 
-previewImgFunc() {
-    args=$@
+img() {
     local MIN_CELL_W=20
     local MIN_CELL_H=$(( MIN_CELL_W / 2))
 
@@ -12,7 +11,8 @@ previewImgFunc() {
     local MAX_ROWS=$(( termHeight / MIN_CELL_H ))
 
     local fileCount cols rows
-    fileCount=$(find . -maxdepth 1 -type f | wc -l)
+    #fileCount=$(find . -maxdepth 1 -type f | wc -l)
+    fileCount=$(find . -maxdepth 1 \( -type f -o -iname '*.png' -o -iname '*.jpg' -o -iname '*.webp' -o -iname '*.jpeg' -o -iname '*.avif' -o -iname '*.ico' -o -iname '*.bmp' \)| wc -l)
     cols=$(( fileCount < MAX_COLS ? fileCount : MAX_COLS ))
     rows=$(( (fileCount + cols - 1) / cols ))
     rows=$(( rows < MAX_ROWS ? rows : MAX_ROWS ))
@@ -25,7 +25,5 @@ previewImgFunc() {
     local x=$(( ( termWidth / ( MAX_COLS / cols ) ) ))
     local y=$(( termHeight / rows ))
 
-    timg ./* --upscale=i --center --title --grid="${cols}x${rows}" -g"${x}x${y}" -b "#FFFFFF" -B "#000000" 2>/dev/null
+    timg ./* --upscale=i --fit-width --center --title --grid="${cols}x${rows}" -g"${x}x${y}" -b "#FFFFFF" -B "#000000" 2>/dev/null
 }
-
-alias img="previewImgFunc"
