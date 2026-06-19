2df105fb93a70c31d5a9ea2bb0c29d1fc05185e3
librepup <zweitlaender@gmail.com>
2026-06-19 07:46:29 +0200

    Added comfyui-launch ZSH Function

--- files/scripts/comfyui.sh
+++ files/scripts/comfyui.sh
@@ -1,19 +1,22 @@
 #!/usr/bin/env bash
 
-podman run -it --rm \
-  --name comfyui-cu126 \
-  --gpus all --device nvidia.com/gpu=all \
-  -p 8188:8188 \
-  -v /extra/ComfyUI/storage-cache/dot-cache:/root/.cache \
-  -v /extra/ComfyUI/storage-cache/dot-config:/root/.config \
-  -v /extra/ComfyUI/storage-nodes/dot-local:/root/.local \
-  -v /extra/ComfyUI/storage-nodes/custom_nodes:/root/ComfyUI/custom_nodes \
-  -v /extra/ComfyUI/storage-models/models:/root/ComfyUI/models \
-  -v /extra/ComfyUI/storage-models/hf-hub:/root/.cache/huggingface/hub \
-  -v /extra/ComfyUI/storage-models/torch-hub:/root/.cache/torch/hub \
-  -v /extra/ComfyUI/storage-user/input:/root/ComfyUI/input \
-  -v /extra/ComfyUI/storage-user/output:/root/ComfyUI/output \
-  -v /extra/ComfyUI/storage-user/user-profile:/root/ComfyUI/user \
-  -v /extra/ComfyUI/storage-user/user-scripts:/root/user-scripts \
-  -e CLI_ARGS="" \
-  yanwk/comfyui-boot:cu126-slim
+comfyui-launch() {
+  xdg-open "http://0.0.0.0:8188"
+  podman run -it --rm \
+    --name comfyui-cu126 \
+    --gpus all --device nvidia.com/gpu=all \
+    -p 8188:8188 \
+    -v /extra/ComfyUI/storage-cache/dot-cache:/root/.cache \
+    -v /extra/ComfyUI/storage-cache/dot-config:/root/.config \
+    -v /extra/ComfyUI/storage-nodes/dot-local:/root/.local \
+    -v /extra/ComfyUI/storage-nodes/custom_nodes:/root/ComfyUI/custom_nodes \
+    -v /extra/ComfyUI/storage-models/models:/root/ComfyUI/models \
+    -v /extra/ComfyUI/storage-models/hf-hub:/root/.cache/huggingface/hub \
+    -v /extra/ComfyUI/storage-models/torch-hub:/root/.cache/torch/hub \
+    -v /extra/ComfyUI/storage-user/input:/root/ComfyUI/input \
+    -v /extra/ComfyUI/storage-user/output:/root/ComfyUI/output \
+    -v /extra/ComfyUI/storage-user/user-profile:/root/ComfyUI/user \
+    -v /extra/ComfyUI/storage-user/user-scripts:/root/user-scripts \
+    -e CLI_ARGS="" \
+    yanwk/comfyui-boot:cu126-slim
+}
--- modules/home/files.nix
+++ modules/home/files.nix
@@ -134,6 +134,7 @@
     # Completions (comps)
     ".shell-autoload-functions/comps/_g".source = ../../files/completions/_g;
     # Functions (funcs)
+    ".shell-autoload-functions/funcs/comfyui.sh".source = ../../files/scripts/comfyui.sh;
     ".shell-autoload-functions/funcs/netRoute.sh".source = ../../files/scripts/netRoute.sh;
     ".shell-autoload-functions/funcs/restartPortals.sh".source = ../../files/scripts/restartPortals.sh;
     ".shell-autoload-functions/funcs/convertToPdf.sh".source = ../../files/scripts/convertToPdf.sh;
