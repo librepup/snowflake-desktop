8369d47027c8a8ff128abd4a60011c40dc2fcfd0
librepup <zweitlaender@gmail.com>
2026-06-21 05:36:55 +0200

    Added Shondo Content and Revised vidToGif Script

--- files/scripts/vidToGif.sh
+++ files/scripts/vidToGif.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-arg=$1
+ffmpeg -i "$1" "${1}".gif
 
-ffmpeg -i $1 -vf "fps=10,scale=320:-1:flags=lanczos" -loop 0 ${arg}.gif
+# ffmpeg -i $1 -vf "fps=10,scale=320:-1:flags=lanczos" -loop 0 ${arg}.gif
