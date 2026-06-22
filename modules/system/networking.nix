c7aeb2821750b95a1cc6ea36b208b0642532bd17
librepup <zweitlaender@gmail.com>
2026-06-22 19:32:08 +0200

    Updated NetworkManager Dispatch Script

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -22,23 +22,32 @@
         "1.1.1.1"
         "8.8.8.8"
       ];
+      dispatcherScripts = [
+        {
+          source = pkgs.writeShellScript "connectivityUpdateHook" ''
+            #!/bin/sh
+
+            if [ "$2" = "up" ] || [ "$2" = "down" ] || [ "$2" = "connectivity-change" ]; then
+              case "$1" in
+                enp0s31f6|enp*)
+                  sudo -u "puppy" DISPLAY="$DISPLAY" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "$1" "Connected to $1 after $2" -i /run/current-system/sw/share/icons/breeze-dark/status/24/network-wired.svg
+                  ;;
+                wlp4s0u2|wlp*)
+                  sudo -u "puppy" DISPLAY="$DISPLAY" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "$1" "Connected to $1 after $2" -i /run/current-system/sw/share/icons/breeze-dark/status/24/network-wireless-on.svg
+                  ;;
+                *)
+                  sudo -u "puppy" DISPLAY="$DISPLAY" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "Unknown" "Network Changed to Unknown Mode" -i /etc/nixos/files/pictures/icons/error.png
+                  ;;
+              esac
+            fi
+          '';
+          type = "basic";
+        }
+      ];
     };
     hostName = "snowflake";
-    ### Old Nameservers Snippet, apparently not Required and can Conflict,
-    ### when Nameservers are also set via NetworkManager.
-    # nameservers = [
-    #   "1.1.1.1"
-    #   "8.8.8.8"
-    # ];
     firewall = {
       checkReversePath = lib.mkForce false; # Required to get VPNs like ProtonVPN to Work.
-      ### Old TCP Port Rules
-      # allowedTCPPorts = [
-      #   53317
-      # ];
-      # allowedUDPPorts = [
-      #   53317
-      # ];
     };
   };
 }
