43cffac83dc4322efdd20d008cf8190ab227dadb
librepup <zweitlaender@gmail.com>
2026-06-22 19:40:31 +0200

    Fixed Paths in NetworkManager Dispatch Script

--- modules/system/networking.nix
+++ modules/system/networking.nix
@@ -28,13 +28,13 @@
             if [ "$2" = "up" ] || [ "$2" = "down" ] || [ "$2" = "connectivity-change" ]; then
               case "$1" in
                 enp0s31f6|enp*)
-                  sudo -u "puppy" DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "$1" "Connected to $1 after $2" -i /run/current-system/sw/share/icons/breeze-dark/status/24/network-wired.svg
+                  ${pkgs.sudo}/bin/sudo -u "puppy" DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(${pkgs.coreutils}/bin/id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "$1" "Connected to $1 after $2" -i /run/current-system/sw/share/icons/breeze-dark/status/24/network-wired.svg
                   ;;
                 wlp4s0u2|wlp*)
-                  sudo -u "puppy" DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "$1" "Connected to $1 after $2" -i /run/current-system/sw/share/icons/breeze-dark/status/24/network-wireless-on.svg
+                  ${pkgs.sudo}/bin/sudo -u "puppy" DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(${pkgs.coreutils}/bin/id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "$1" "Connected to $1 after $2" -i /run/current-system/sw/share/icons/breeze-dark/status/24/network-wireless-on.svg
                   ;;
                 *)
-                  sudo -u "puppy" DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "Unknown" "Network Changed to Unknown Mode" -i /etc/nixos/files/pictures/icons/error.png
+                  ${pkgs.sudo}/bin/sudo -u "puppy" DISPLAY=":0" DBUS_SESSION_BUS_ADDRESS="unix:path=/run/user/$(${pkgs.coreutils}/bin/id -u puppy)/bus" ${pkgs.libnotify}/bin/notify-send "Unknown" "Network Changed to Unknown Mode" -i /etc/nixos/files/pictures/icons/error.png
                   ;;
               esac
             fi
