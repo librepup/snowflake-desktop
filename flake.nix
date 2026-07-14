2d981a641d0785545b1a6e7b41eb1f8106253813
librepup <zweitlaender@gmail.com>
2026-07-14 14:34:33 +0200

    Updated NixVim PKG Pointer

--- flake.nix
+++ flake.nix
@@ -21,7 +21,6 @@
     };
     nixvim = {
       url = "github:nix-community/nixvim/nixos-26.05";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     noctalia = {
       url = "github:librepup/noctalia-shell";
