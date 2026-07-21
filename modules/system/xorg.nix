220f1fcfb2e123a20c9511d74a56af6a50a73559
librepup <zweitlaender@gmail.com>
2026-07-21 16:03:54 +0200

    Cleaned

--- modules/system/xorg.nix
+++ modules/system/xorg.nix
@@ -1,27 +1,27 @@
 { config, pkgs, inputs, ... }:
-let
-  hevelPkg = inputs.neu-nix.packages.x86_64-linux.hevel;
-  neuswcPkg = inputs.neu-nix.packages.x86_64-linux.neuswc;
-  tohuPkg = inputs.neu-nix.packages.x86_64-linux.tohu;
-  hevelSession = (pkgs.writeTextDir "share/wayland-sessions/hevel.desktop" ''
-    [Desktop Entry]
-    Name=hevel
-    Comment=Custom Hevel WM Session
-    Exec=${neuswcPkg}/bin/swc-launch ${hevelPkg}/bin/hevel
-    Type=Application
-  '').overrideAttrs (oldAttrs: {
-    passthru.providedSessions = [ "hevel" ];
-  });
-  tohuSession = (pkgs.writeTextDir "share/wayland-sessions/tohu.desktop" ''
-    [Desktop Entry]
-    Name=tohu
-    Comment=Custom Tohu WM Session
-    Exec=${neuswcPkg}/bin/swc-launch ${tohuPkg}/bin/tohu
-    Type=Application
-  '').overrideAttrs (oldAttrs: {
-    passthru.providedSessions = [ "tohu" ];
-  });
-in
+#let
+  #hevelPkg = inputs.neu-nix.packages.x86_64-linux.hevel;
+  #neuswcPkg = inputs.neu-nix.packages.x86_64-linux.neuswc;
+  #tohuPkg = inputs.neu-nix.packages.x86_64-linux.tohu;
+  #hevelSession = (pkgs.writeTextDir "share/wayland-sessions/hevel.desktop" ''
+  #  [Desktop Entry]
+  #  Name=hevel
+  #  Comment=Custom Hevel WM Session
+  #  Exec=${neuswcPkg}/bin/swc-launch ${hevelPkg}/bin/hevel
+  #  Type=Application
+  #'').overrideAttrs (oldAttrs: {
+  #  passthru.providedSessions = [ "hevel" ];
+  #});
+  #tohuSession = (pkgs.writeTextDir "share/wayland-sessions/tohu.desktop" ''
+  #  [Desktop Entry]
+  #  Name=tohu
+  #  Comment=Custom Tohu WM Session
+  #  Exec=${neuswcPkg}/bin/swc-launch ${tohuPkg}/bin/tohu
+  #  Type=Application
+  #'').overrideAttrs (oldAttrs: {
+  #  passthru.providedSessions = [ "tohu" ];
+  #});
+#in
 {
   environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text =
     builtins.toJSON {
